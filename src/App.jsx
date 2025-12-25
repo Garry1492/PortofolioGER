a0a4bb61fb2b54e4f136844accf945c1bbc9cfd2
Garry1492 <115008640+Garry1492@users.noreply.github.com>
2025-12-25 12:26:07 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"; 
 import BackToTop from "./components/BackToTop";
 import 'remixicon/fonts/remixicon.css'
+import 'animate.css';
 
 function App() {
   const [loading, setLoading] = useState(true);
